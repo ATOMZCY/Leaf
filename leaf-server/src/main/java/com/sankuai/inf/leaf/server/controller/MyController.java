2dba7b4026fb7c1b49dcd6d9b985c60d0871ccec
zhuchunyi001 <zhuchunyi001@ke.com>
2019-11-19 16:32:45 +0800

    test push

---
+++ leaf-server/src/main/java/com/sankuai/inf/leaf/server/controller/MyController.java
@@ -0,0 +1,12 @@
+package com.sankuai.inf.leaf.server.controller;
+
+/**
+ * <p>
+ * Description:test push
+ * </p>
+ *
+ * @author zcy
+ * @date 2019/11/19 16:19
+ */
+public class MyController {
+}
